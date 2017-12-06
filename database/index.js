e9fe22a7bd20c52d275e829c5c5d98a161ba649d
Neha Chaudhary <neha.chaudhary@hotmail.com>
2017-12-06 12:57:58 -0800

    Clean up code

--- database/index.js
+++ database/index.js
@@ -23,10 +23,6 @@ if (process.env.DB_PORT) {
   dbOptions.port = Number(process.env.DB_PORT);
 }
 
-// const db = new Sequelize('gi4gtv1icfdevbnt', 'lbvk1eybxp69zwhb', 'iyif1vfodnwe09x6', {
-//   host: 'lmag6s0zwmcswp5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//   port: 3306
-
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, dbOptions);
 
 //---------SCHEMA DEFINITIONS--------------------
@@ -137,18 +133,6 @@ Photos.sync();
 Notifications.sync();
 PlacesOfInterest.sync();
 Chat.sync();
-
-
-//Promises do not work!!!!
-// Users.sync()
-//   .then(() => UserTrip.sync())
-//   .then(() => Trips.sync())
-//   .then(() => Votes.sync())
-//   .then(() => Landmarks.sync())
-//   .then(() => Expenses.sync())
-//   .then(() => Sessions.sync())
-//   // .then(() => Photos.sync())
-//   .catch(err => console.log('error syncing database', err));
 
 //--------------------FOREIGN KEY SETTINGS -----------------
 
